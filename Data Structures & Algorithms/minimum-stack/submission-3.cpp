b10171e95f4f06d7659ab6153e654a0533b43782
Bal Krishan Kumar <147535375+coderkrishna45@users.noreply.github.com>
2026-05-11 11:34:01 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,20 @@
+class MinStack {
+   public:
+    MinStack() {}
+
+    vector<pair<int, int>> v;
+
+    void push(int val) {
+        if (v.empty()) {
+            v.push_back({val, val});
+        } else {
+            v.push_back({val, min(val, v.back().second)});
+        }
+    }
+
+    void pop() { v.pop_back(); }
+
+    int top() { return v.back().first; }
+
+    int getMin() { return v.back().second; }
+};
